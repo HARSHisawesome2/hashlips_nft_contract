47b2ee2aec4fafef7daf4439e60dca149b5f9e53
HARSHisawesome2 <103326606+HARSHisawesome2@users.noreply.github.com>
2022-06-24 21:23:12 +0530

    Update SimpleNft.sol

--- contract/SimpleNft.sol
+++ contract/SimpleNft.sol
@@ -19,26 +19,26 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFT is ERC721Enumerable, Ownable {
-  using Strings for uint256;
+   using Counters for Counters.Counter;
+   IERC20 public tokenAddress;
+   uint256 public rate = 100 * 10 ** 18;
+   using Strings for uint256;
 
   string baseURI;
   string public baseExtension = ".json";
-  uint256 public cost = 0.05 ether;
-  uint256 public maxSupply = 10000;
-  uint256 public maxMintAmount = 20;
-  bool public paused = false;
-  bool public revealed = false;
-  string public notRevealedUri;
+  
+  Counters.Counter private _tokenIdCounter;
 
-  constructor(
+  constructor{
     string memory _name,
     string memory _symbol,
+    tokenAddress = IERC20(_tokenAddress);
     string memory _initBaseURI,
-    string memory _initNotRevealedUri
-  ) ERC721(_name, _symbol) {
+
+  } ERC721(_name, _symbol) (
     setBaseURI(_initBaseURI);
-    setNotRevealedURI(_initNotRevealedUri);
-  }
+  
+  
 
   // internal
   function _baseURI() internal view virtual override returns (string memory) {
@@ -46,20 +46,12 @@ contract NFT is ERC721Enumerable, Ownable {
   }
 
   // public
-  function mint(uint256 _mintAmount) public payable {
+  function mint(uint256 _mintAmount) public  {
     uint256 supply = totalSupply();
-    require(!paused);
-    require(_mintAmount > 0);
-    require(_mintAmount <= maxMintAmount);
-    require(supply + _mintAmount <= maxSupply);
-
-    if (msg.sender != owner()) {
-      require(msg.value >= cost * _mintAmount);
-    }
-
-    for (uint256 i = 1; i <= _mintAmount; i++) {
-      _safeMint(msg.sender, supply + i);
-    }
+    tokenAddress.transferFrom(msg.sender, address(this), rate);
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(msg.sender, tokenId);
   }
 
   function walletOfOwner(address _owner)
@@ -87,9 +79,7 @@ contract NFT is ERC721Enumerable, Ownable {
       "ERC721Metadata: URI query for nonexistent token"
     );
     
-    if(revealed == false) {
-        return notRevealedUri;
-    }
+ 
 
     string memory currentBaseURI = _baseURI();
     return bytes(currentBaseURI).length > 0
@@ -101,18 +91,9 @@ contract NFT is ERC721Enumerable, Ownable {
   function reveal() public onlyOwner {
       revealed = true;
   }
-  
-  function setCost(uint256 _newCost) public onlyOwner {
-    cost = _newCost;
-  }
 
-  function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
-    maxMintAmount = _newmaxMintAmount;
-  }
   
-  function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
-    notRevealedUri = _notRevealedURI;
-  }
+
 
   function setBaseURI(string memory _newBaseURI) public onlyOwner {
     baseURI = _newBaseURI;
@@ -126,19 +107,7 @@ contract NFT is ERC721Enumerable, Ownable {
     paused = _state;
   }
  
-  function withdraw() public payable onlyOwner {
-    // This will pay HashLips 5% of the initial sale.
-    // You can remove this if you want, or keep it in to support HashLips and his channel.
-    // =============================================================================
-    (bool hs, ) = payable(0x943590A42C27D08e3744202c4Ae5eD55c2dE240D).call{value: address(this).balance * 5 / 100}("");
-    require(hs);
-    // =============================================================================
-    
-    // This will payout the owner 95% of the contract balance.
-    // Do not remove this otherwise you will not be able to withdraw the funds.
-    // =============================================================================
-    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
-    require(os);
-    // =============================================================================
-  }
+ function withdrawToken() public onlyOwner {
+        tokenAddress.transfer(msg.sender, tokenAddress.balanceOf(address(this)));
+    }
 }
